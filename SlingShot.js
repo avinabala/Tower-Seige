494b8f2c1a65158842317ca14e858e58308059e7
avinabala <78329570+avinabala@users.noreply.github.com>
2021-04-28 23:10:33 -0400

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,30 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.06,
+            length: 20
+        }
+        this.pointB = pointB
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    fly(){
+        this.slingshot.bodyA = null
+    }
+   attach(body){
+     this.slingshot.bodyA = body
+   }
+    display(){
+        if(this.slingshot.bodyA){
+        
+        var pointA = this.slingshot.bodyA.position;
+        var pointB = this.pointB;
+
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+ }
+}
